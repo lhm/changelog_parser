ca015c4b113422e267998ab4c5246bdc429a79e5
Lars Henrik Mai <lars.mai@kontinui.de>
2011-06-23 19:07:00 +0200

    added support for different formats

--- lib/changelog-parser.rb
+++ lib/changelog-parser.rb
@@ -2,25 +2,21 @@ require 'date'
 
 module ChangelogParser
 
-  RE_SECTION_START = /^(?=\={3})/
-  RE_HEADLINE      = /#{RE_SECTION_START}.+$/
-  RE_LINE_SEP      = /\n(?:\s*)?/
-  RE_SEMVER        = /\d+.\d+.\d/
-
   class Reader
 
     attr_accessor :changelog
 
-    def initialize(changelog_string)
+    def initialize(changelog_string, format)
       @changelog = changelog_string
+      @format    = format
     end
 
-    def self.read(path)
-      self.new(File.read(path))
+    def self.read(path, format)
+      self.new(File.read(path), format)
     end
 
     def versions
-      @versions ||= @changelog.split(RE_SECTION_START).map {|section| Version.new(section)}
+      @versions ||= @changelog.split(@format.section_start).map {|section| Version.new(section, @format)}
     end
 
   end
@@ -30,25 +26,98 @@ module ChangelogParser
 
     attr_accessor :text
 
-    def initialize(section_string)
-      @text = section_string
+    def initialize(section_string, format)
+      @text   = section_string
+      @format = format
     end
 
     def lines
-      @lines ||= @text.split(RE_LINE_SEP)
+      @lines ||= @text.split(@format.line_seperator)
     end
 
     def headline
-      @headline ||= @text[RE_HEADLINE]
+      @headline ||= @text[@format.headline]
     end
 
     def number
-      @number ||= lines.first[RE_SEMVER]
+      @number ||= headline[@format.number]
     end
 
     def release_date
-      @release_date ||= Date.parse(headline)
+      @release_date ||= if @format.release_date
+                         headline[@format.release_date]
+                        else
+                          begin
+                            Date.parse(headline)
+                          rescue ArgumentError # date unparseable
+                            nil
+                          end
+                        end 
     end
+
+  end
+
+  module Formats
+
+    class Format
+
+      RE_SEMVER = /\d+.\d+.\d/
+
+      def self.section_start;  raise NotImplementedError; end
+      def self.line_seperator; raise NotImplementedError; end
+      def self.headline;       raise NotImplementedError; end
+      def self.number;         raise NotImplementedError; end
+      def self.release_date;   raise NotImplementedError; end
+    end
+
+    class Hoe < Format
+
+      def self.section_start
+        /^(?=\={3})/
+      end
+
+      def self.line_seperator
+        /\n(?:\s*)?/
+      end
+
+      def self.headline
+        /#{self.section_start}.+$/
+      end
+
+      def self.number
+        RE_SEMVER
+      end
+
+      def self.release_date
+        nil
+      end
+
+    end
+
+    class Rails < Format
+
+      def self.section_start
+        /^(?=\*(?:Rails|#{self.number}))/
+      end
+
+      def self.line_seperator
+        /\n+(?=\*)/
+      end
+
+      def self.headline
+        /#{self.section_start}.+$/
+      end
+
+      def self.number
+        RE_SEMVER
+      end
+
+      def self.release_date
+        nil
+      end
+
+    end
+
 
   end
 
