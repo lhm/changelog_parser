e97365d3c21083fe566d331859ced34305eb910b
Lars Henrik Mai <lars.mai@kontinui.de>
2011-07-02 22:43:09 +0200

    refactor cli class

--- lib/changelog_parser/cli.rb
+++ lib/changelog_parser/cli.rb
@@ -4,32 +4,33 @@ module ChangelogParser
 
   class CLI
 
-    attr_reader :opts, :reader
-
     def initialize
       opts = Slop.new :help => true do
         on :n, :number,  'number of entries to print', true, :as => Integer, :default => 1
-        on :f, :file,    'path to the changelog file to parse; defaults to STDIN if not given', true
-        # on :f, :format,  'specify format of changelog for parsing; currently supported: default, rails.'
       end
 
-      @opts = opts.parse.to_hash
-
-      @reader = ChangelogParser::Reader.new(get_text)
-
+      @filename = extract_args(opts).first
+      @opts     = opts.to_hash
+      @reader   = ChangelogParser::Reader.new(get_text)
     end
 
     def run
-      entries = reader.entries[0..n-1]
-      puts entries.map(&:text).join('\n')
+      entries = @reader.entries[0..@opts['number']-1]
+      puts entries.map(&:text).join("\n")
     end
 
     def get_text
-      if opts['file'].nil?
+      if @filename.nil?
         STDIN.gets(nil)
       else
-        File.read(opts['file'])
+        File.read(@filename)
       end
+    end
+
+    def extract_args(opts)
+      args = []
+      opts.parse {|arg| args << arg}
+      args
     end
 
   end
