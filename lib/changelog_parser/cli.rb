a74680ac43d13629114f835e1ccaac7e8cbd2c6a
Lars Henrik Mai <lars.mai@kontinui.de>
2011-07-03 15:17:46 +0200

    restructured cli class

--- lib/changelog_parser/cli.rb
+++ lib/changelog_parser/cli.rb
@@ -2,35 +2,39 @@ require 'slop'
 
 module ChangelogParser
 
-  class CLI
+  class Runner
 
     def initialize
-      opts = Slop.new :help => true do
-        on :n, :number,  'number of entries to print', true, :as => Integer, :default => 1
-      end
-
-      @filename = extract_args(opts).first
-      @opts     = opts.to_hash
-      @reader   = ChangelogParser::Reader.new(get_text)
     end
 
     def run
-      entries = @reader.entries[0..@opts['number']-1]
+      entries = reader.entries[0..@opts['number']-1]
       puts entries.map(&:text).join("\n")
     end
 
+    def reader
+      @reader ||= ChangelogParser::Reader.new(get_text)
+    end
+
+    def parse_options(args)
+      opts = Slop.new :help => true do
+        on :n, :number,  'number of entries to print', true, :as => Integer, :default => 1
+      end
+      filenames = []
+      opts.parse {|arg| filenames << arg}
+      @filename, @opts = [filenames.first, opts.to_hash]
+    end
+
     def get_text
-      if @filename.nil?
+      if from_stdin?
         STDIN.gets(nil)
       else
         File.read(@filename)
       end
     end
 
-    def extract_args(opts)
-      args = []
-      opts.parse {|arg| args << arg}
-      args
+    def from_stdin?
+      @filename.nil?
     end
 
   end
